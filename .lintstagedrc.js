4e401558c563bc9944e28909e69a7cc762e20123
sasaplus1 <sasaplus1@gmail.com>
2020-11-06 02:02:51 +0900

    lint when commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,8 @@
 module.exports = {
+  '*.{js,ts}': 'eslint --cache --ext .js,.ts',
   'package.json': [
     'npx fixpack',
+    'prettier --parser json-stringify --write',
     'git diff --exit-code --quiet'
   ],
   'package-lock.json': 'node -e "process.exitCode = 1;"'
